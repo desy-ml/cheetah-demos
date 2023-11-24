48133a33b6d2f7ac6c7afd0c584074e340f00fd8
Jan Kaiser <jan.kaiser.email@googlemail.com>
2023-11-24 16:03:57 +0100

    Setup data generation SLURM script

---
+++ neural_network_space_charge_quad/generate_data_slurm.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#SBATCH --partition=maxcpu
+#SBATCH --job-name generate-data-space-charge-quad
+#SBATCH --time=1-00:00:00
+#SBATCH --nodes=1
+#SBATCH --constraint=75F3
+#SBATCH --mail-type ALL
+
+source /etc/profile.d/modules.sh
+
+source ~/.bashrc
+conda activate cheetah-demos
+cd /home/kaiserja/beegfs/cheetah-demos/neural_network_space_charge_quad
+
+srun python generate_data.py 6000 data/train
+
+exit
