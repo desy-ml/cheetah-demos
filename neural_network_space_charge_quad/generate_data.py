b1e1180989bb31151af9539ade634c6fcda7a136
Jan Kaiser <jan.kaiser.email@googlemail.com>
2023-11-24 15:09:55 +0100

    Definte data generation with full parameter sets

--- neural_network_space_charge_quad/generate_data.py
+++ neural_network_space_charge_quad/generate_data.py
@@ -18,31 +18,33 @@ def generate_sample(idx: int) -> None:
     :param idx: Unique index of the sample.
     """
     # Generate control values
-    length = np.random.uniform(0.05, 0.5)  # 0.2
-    k1 = np.random.uniform(-72.0, 72.0)  # 4.2
-    x_offs = np.random.uniform(-0.5e-3, 0.5e-3)
-    y_offs = np.random.uniform(-0.5e-3, 0.5e-3)
-    tilt = np.random.uniform(-0.1, 0.1)  # 0.0
+    length = np.random.uniform(0.05, 0.5)
+    k1 = np.random.uniform(-72.0, 72.0)
 
     # Create Ocelot cell
-    quadrupole = ocelot.XYQuadrupole(
-        l=length, k1=k1, x_offs=x_offs, y_offs=y_offs, tilt=tilt
-    )
+    quadrupole = ocelot.Quadrupole(l=length, k1=k1)
     cell = [quadrupole]
 
     # Create Ocelot beam
     p_array_incoming = generate_parray(
-        sigma_x=np.random.uniform(1e-5, 1e-3),  # 1e-4
-        sigma_px=np.random.uniform(1e-5, 1e-3),  # 2e-5
-        sigma_y=np.random.uniform(1e-5, 1e-3),  # None
-        sigma_py=np.random.uniform(1e-5, 1e-3),  # None
-        sigma_tau=np.random.uniform(1e-4, 1e-2),  # 1e-3
-        sigma_p=np.random.uniform(1e-5, 1e-3),  # 1e-4
-        chirp=np.random.uniform(-0.1, 0.1),  # 0.01
-        charge=np.random.uniform(1e-9, 1e-7),  # 5e-9
-        nparticles=np.random.randint(100_000, 1_000_000),  # 200_000
-        energy=np.random.uniform(0.001, 15.0),  # 0.13 (I think this is in GeV)
+        sigma_x=np.random.uniform(1e-5, 1e-3),
+        sigma_px=np.random.uniform(1e-5, 1e-3),
+        sigma_y=np.random.uniform(1e-5, 1e-3),
+        sigma_py=np.random.uniform(1e-5, 1e-3),
+        sigma_tau=np.random.uniform(3e-7, 3e-4),
+        sigma_p=np.random.uniform(1e-5, 1e-3),
+        charge=np.random.uniform(1e-12, 5e-9),
+        nparticles=np.random.randint(100_000),
+        energy=np.random.uniform(0.001, 1.0),  # GeV
     )
+    mu_x = np.random.uniform(-1e-3, 1e-3)
+    mu_xp = np.random.uniform(-1e-6, 1e-6)
+    mu_y = np.random.uniform(-1e-3, 1e-3)
+    mu_yp = np.random.uniform(-1e-6, 1e-6)
+    p_array_incoming.rparticles[0] += mu_x
+    p_array_incoming.rparticles[1] += mu_xp
+    p_array_incoming.rparticles[2] += mu_y
+    p_array_incoming.rparticles[3] += mu_yp
 
     # Run tracking
     method = {"global": ocelot.SecondTM}
@@ -63,13 +65,7 @@ def generate_sample(idx: int) -> None:
     sample_dir = Path(f"data/{idx:09d}")
     sample_dir.mkdir(parents=True, exist_ok=True)
     with open(sample_dir / "controls.yaml", "w") as f:
-        controls_dict = {
-            "length": length,
-            "k1": k1,
-            "x_offs": x_offs,
-            "y_offs": y_offs,
-            "tilt": tilt,
-        }
+        controls_dict = {"length": length, "k1": k1}
         yaml.dump(controls_dict, f)
     ocelot.save_particle_array(sample_dir / "incoming.npz", p_array_incoming)
     ocelot.save_particle_array(sample_dir / "outgoing.npz", p_array_outgoing)
